a187f4b6dbdb717e48ca1d04fbb1bf9980e70af4
AndreaC05 <131610224+AndreaC05@users.noreply.github.com>
2023-04-25 15:57:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let menuVisible = false;
+//funcion que oculta o muestra menu
+function mostrarOcultarMenu(){
+	if (menuVisible) {
+		document.getElementById("nav").classList ="";
+		menuVisible = false;
+	}else{
+		document.getElementById("nav").classList ="responsive";
+		menuVisible = true;
+	}
+}
+
+function seleccionar(){
+	//oculto el menu una vez que selecciono una opcion
+	document.getElementById("nav").classList ="";
+	menuVisible = false;
+}
+
+//funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+	var skills = document.getElementById("skills");
+	var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+	if (distancia_skills >= 300) {
+		let habilidades = document.getElementsByClassName("progreso");
+		habilidades[0].classList.add("boostrap");
+		habilidades[1].classList.add("htmlcss");
+		habilidades[2].classList.add("python");
+		habilidades[3].classList.add("wordpress");
+		habilidades[4].classList.add("comunicacion");
+		habilidades[5].classList.add("trabajo");
+		habilidades[6].classList.add("creatividad");
+		habilidades[7].classList.add("dedicacion");
+	}
+}
+//detecto el strolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+	efectoHabilidades();
+}
+
+document.getElementById('btn-descargar').addEventListener('click', function() {
+    var link = document.createElement('a');
+    link.href = 'file:///E:/III%20SEMESTRE/portafolio/archivo/Andrea_Aguirre_Ruiz_CV.pdf';
+    link.download = 'Andrea_Aguirre_Ruiz_CV.pdf';
+    link.target = '_blank';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  });
